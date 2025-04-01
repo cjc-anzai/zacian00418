099520bb55be35141555bc64ba3446332894bb2e
Kaito Anzai <kaito-anzai@cresco-jcube.co.jp>
2025-04-01 14:24:07 +0900

    changes for amplify

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import zacian from "./assets/zacian.jfif";
 import "./App.css";
 
 function App() {
-  const technique = () => {
-    alert("きょじゅうざん");
-  };
+  // const technique = () => {
+  //   alert("きょじゅうざん");
+  // };
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={zacian} className="zacian" alt="zacian" onClick={technique}/>
+      {!isVisible &&<img src={zacian} className="zacian" alt="zacian" onClick={technique}/>}
+      {isVisible && <img src={kyojuzan} alt="GIF Animation" className="visible-gif" />}
 
         <h1>ザシアン</h1>
+
       </header>
     </div>
   );
