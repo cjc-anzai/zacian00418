07366879ae7bd0f0e595baae8623bfcb6083d377
Kaito Anzai <kaito-anzai@cresco-jcube.co.jp>
2025-04-01 14:02:25 +0900

    changes for amplify

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import zacian from "./assets/zacian.jfif";
 import "./App.css";
 
 function App() {
-  document.getElementsByClassName("zacian").addEventListener("click", function(){
+  const technique = () => {
     alert("きょじゅうざん");
-  });
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={zacian} className="zacian" alt="zacian" />
+        <img src={zacian} className="zacian" alt="zacian" onClick={technique}/>
 
         <h1>ザシアン</h1>
       </header>
