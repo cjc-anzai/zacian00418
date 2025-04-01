6fbfa0e527e41f69175001a5793b4e28d0868b43
Kaito Anzai <kaito-anzai@cresco-jcube.co.jp>
2025-04-01 16:02:42 +0900

    changes for amplify

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   const [isFadingOut1, setIsFadingOut1] = useState(false);
   const [isVisible2, setIsVisible2] = useState(false);
   const [isFadingOut2, setIsFadingOut2] = useState(false);
-  // const [isVisible3, setIsVisible3] = useState(false);
+  const [isVisible3, setIsVisible3] = useState(false);
   // const [isFadingOut3, setIsFadingOut3] = useState(false);
 
   const technique1 = () => {
@@ -31,6 +31,7 @@ function App() {
 
   const habayakukami = () => {
     setIsVisible2(true); 
+    setIsVisible3(true); 
     // setIsFadingOut2(false);
   };
 
@@ -38,37 +39,50 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      {!isVisible1 &&(
-        <div className="button-container">
-          <div>
-            <img src={zacian} className="zacian" alt="zacian" onClick={technique1}/>
-            <h1>ザシアンが現れた！</h1>
+      <div className="flex">
+        {!isVisible1 &&(
+          <div className="button-container">
+            <div>
+              <img src={zacian} className="zacian" alt="zacian" onClick={technique1}/>
+              <h1>ザシアンが現れた！</h1>
+            </div>
+            <div>
+              {!isVisible2 &&(
+                <div>
+                  <h1>どうする？</h1>
+                  <button onClick={habayakukami}>ハバタクカミを出す</button>
+                  {/* <button onClick={houou}>ホウオウをだす</button> */}
+                </div>
+              )}
+            </div>
           </div>
-          <div>
-            <h1>どうする？</h1>
-            <button onClick={habayakukami}>ハバタクカミを出す</button>
-            {/* <button onClick={houou}>ホウオウをだす</button> */}
-          </div>
-        </div>
-      )}
-      {isVisible1 && (
-        <div className={`content ${isFadingOut1 ? "fade-out" : ""}`}>
-          <img src={kyojuzan} alt="GIF Animation" className="kyojuzan" />
-          <h1>きょじゅうざん</h1>
-        </div>        
-      )}
-      {isVisible2 && (
-        <div className={`content ${isFadingOut2 ? "fade-out" : ""}`}>
-          <img src={habatakukami} alt="GIF Animation" className="habatakukami" />
-          <h1>ハバタクカミ</h1>
-        </div>        
-      )}
-      {/* {isVisible3 && (
-        <div className={`content ${isFadingOut3 ? "fade-out" : ""}`}>
-          <img src={houou} alt="GIF Animation" className="houou" />
-          <h1>ホウオウ</h1>
-        </div>        
-      )} */}
+        )}
+        {isVisible1 && (
+          <div className={`content ${isFadingOut1 ? "fade-out" : ""}`}>
+            <img src={kyojuzan} alt="GIF Animation" className="kyojuzan" />
+            <h1>きょじゅうざん</h1>
+          </div>        
+        )}
+        {isVisible3 && (
+          <div className={`content ${isFadingOut2 ? "fade-out" : ""}`}>
+            <div>
+              <img src={habatakukami} alt="GIF Animation" className="habatakukami" />
+              <h1>ハバタクカミ</h1>
+            </div>
+            <div>
+              <h1>どうする？</h1>
+              <button onClick={moonForce}>ムーンフォース</button>
+              {/* <button onClick={houou}>ホウオウをだす</button> */}
+            </div>
+          </div>        
+        )}
+        {/* {isVisible3 && (
+          <div className={`content ${isFadingOut3 ? "fade-out" : ""}`}>
+            <img src={houou} alt="GIF Animation" className="houou" />
+            <h1>ホウオウ</h1>
+          </div>        
+        )} */}
+      </div>
 
       </header>
     </div>
